f7d6a9274f353abfe3cc2ef4f77c24ba72fca0f7
Snehal Gupta <snehalgupta030@gmail.com>
2025-07-04 15:21:16 +0530

    RED : Added a failing test case to handle delimiters in input string as well

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,5 +22,11 @@ RSpec.describe StringCalculator do
         it 'returns the sum when numbers are separated by newlines as well as commas' do
             expect(calculator.add("1\n2,3")).to eq(6)
         end
+        
+        it 'supports custom single-character delimiter defined in the format //;\n1;2' do
+            expect(calculator.add("//;\n1;2")).to eq(3)
+        end
+        
+
     end 
 end 
