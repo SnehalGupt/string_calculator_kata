142319ed5dc6b8f4255f68a449a929909b36ac1c
Snehal Gupta <snehalgupta030@gmail.com>
2025-07-04 15:13:36 +0530

    green : passing cases for adding mukltiple numbers or rather any count of numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,14 +1,22 @@
 require_relative '../lib/string_calculator'
 RSpec.describe StringCalculator do 
-    let(:calc) { StringCalculator.new }
+    let(:calculator) { StringCalculator.new }
     describe "#add" do
         it "returns 0 for an empty string" do 
             # calc = StringCalculator.new
-            expect(calc.add(" ")).to eq(0)
+            expect(calculator.add(" ")).to eq(0)
         end
         it 'returns the sum of two numbers separated by comma' do
             # calc = StringCalculator.new
-            expect(calc.add("1,2")).to eq(3)
+            expect(calculator.add("1,2")).to eq(3)
         end 
+
+        it 'returns the sum of multiple numbers separated by commas' do
+            expect(calculator.add("1,2,3,4")).to eq(10)
+        end
+
+        it 'returns the sum of an unknown amount of numbers' do
+            expect(calculator.add("2,4,6,8,10")).to eq(30)
+        end
     end 
 end 
