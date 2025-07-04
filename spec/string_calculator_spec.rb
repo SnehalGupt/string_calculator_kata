101b39ba20155a404a2bef35632c45ab22c61f0f
Snehal Gupta <snehalgupta030@gmail.com>
2025-07-04 14:11:45 +0530

    Green - A passing spec for case when we get a 0 for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe StringCalculator do
     describe "#add" do
         it "returns 0 for an empty string" do 
             calc = StringCalculator.new
-            expect(calc.add("").to eq(0))
+            expect(calc.add(" ")).to eq(0)
         end 
     end 
 end 
